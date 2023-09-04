2dcc4acae87ee9ddbf593dc8b6ae4316a1bdb2eb
MHUAMANJORGEALE <mhuamanjorgeale@uss.edu.pe>
2023-09-04 16:40:10 -0500

    estructuras de control - bucles

---
+++ 2. estructuras de control/bucle-foreach.php
@@ -0,0 +1,6 @@
+<?php
+    $estudiantes = array("Maza","Quiroz","Zavala","Santamaria");
+    foreach ($estudiantes as $est) {
+        echo "Nombres: ".$est."<br>";
+    }
+?>
---
+++ 2. estructuras de control/bucle-while.php
@@ -0,0 +1,7 @@
+<?php
+    $contador=1;
+    while ($contador <= 5) {
+        echo "Este ciclo es el ciclo numero $contador <br>";
+        $contador += 1;
+    }
+?>
