8ff67a259a2dc713d4a5c594dae177d7be9adb6a
MHUAMANJORGEALE <mhuamanjorgeale@uss.edu.pe>
2023-09-04 15:44:06 -0500

    estructuras de control

---
+++ 2. estructuras de control/conficion-if.php
@@ -0,0 +1,18 @@
+<?php
+$edad=17;
+
+if($edad>=18){
+    print("Mayor de edad");
+}else{
+    print("Menor de edad");
+}
+
+$numero=23;
+print("<br>");
+if($numero%2 == 0){
+    
+    print("es par");
+}else{
+    print("es inpar");
+}
+?>
